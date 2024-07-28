3f79d8a8c1acd19090f279630c15a6ef36463bd1
Himanshuk14 <hsrreso1234@gmail.com>
2024-07-29 01:57:06 +0530

    Refactor backend

---
+++ api/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+//import userRouter from "./routes/user.route.js";
+//app.use("/users", userRouter);
+
+export default app;
---
+++ api/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
+import connectDB from "./db/index.js";
+import app from "./app.js";
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`Server running in  on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error.message));
