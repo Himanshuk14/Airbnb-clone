e24763bac7f00347c2b760897875f2326fe6f24c
Himanshuk14 <hsrreso1234@gmail.com>
2024-07-30 01:36:18 +0530

    Add the addplace controller

--- api/src/controllers/place.controller.js
+++ api/src/controllers/place.controller.js
@@ -2,11 +2,12 @@ import { User } from "../models/users.model.js";
 import { Place } from "../models/places.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const addPlaces = asyncHandler(async (req, res) => {
   const {
     address,
-    photos,
     perks,
     extraInfo,
     checkIn,
@@ -15,6 +16,9 @@ const addPlaces = asyncHandler(async (req, res) => {
     price,
     title,
   } = req.body;
+
+  let urlArray = [];
+
   if (
     req.files &&
     Array.isArray(req.files.photos) &&
@@ -23,11 +27,50 @@ const addPlaces = asyncHandler(async (req, res) => {
     for (let individual of req.files.photos) {
       //console.log(individual.path);
       const uploaded = await uploadOnCloudinary(individual.path);
-      console.log("uploaded", uploaded.url);
+      urlArray.push(uploaded.url);
+      //console.log("uploaded", uploaded.url);
     }
   }
+  let coverImageUrl = "";
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    const coverImageFilePath = req.files.coverImage[0].path;
+    const uploaded = await uploadOnCloudinary(coverImageFilePath);
+    coverImageUrl = uploaded.url;
+  }
+  //   console.log("urlArray", urlArray);
+  //   console.log("ccoverImageURL", coverImageUrl);
+  const user = await User.findById(req.user?._id);
+  if (!user) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+  const placeData = {
+    owner: user._id,
+    photos: urlArray,
+    coverImage: coverImageUrl,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+    title,
+    address,
+  };
+  const placeDoc = await Place.create(placeData);
+  if (!placeDoc) {
+    throw new ApiError(
+      500,
+      "Something went wrong in saving the place document"
+    );
+  }
 
-  res.json("ok");
+  res
+    .status(201)
+    .json(new ApiResponse(201, placeDoc, "Place document saved to database"));
 });
 
 export { addPlaces };
