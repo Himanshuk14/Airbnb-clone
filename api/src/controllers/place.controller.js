8d67ec1d531bc11cf8149ea09fd6fff9e542f4d3
Himanshuk14 <hsrreso1234@gmail.com>
2024-07-30 00:43:25 +0530

    Basic clodinary testing

--- api/src/controllers/place.controller.js
+++ api/src/controllers/place.controller.js
@@ -1,8 +1,32 @@
 import { User } from "../models/users.model.js";
 import { Place } from "../models/places.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const addPlaces = asyncHandler(async (req, res) => {
+  const {
+    address,
+    photos,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+    title,
+  } = req.body;
+  if (
+    req.files &&
+    Array.isArray(req.files.photos) &&
+    req.files.photos.length > 0
+  ) {
+    for (let individual of req.files.photos) {
+      //console.log(individual.path);
+      const uploaded = await uploadOnCloudinary(individual.path);
+      console.log("uploaded", uploaded.url);
+    }
+  }
+
   res.json("ok");
 });
 
--- api/src/routes/places.route.js
+++ api/src/routes/places.route.js
@@ -5,6 +5,19 @@ import { addPlaces } from "../controllers/place.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route("/addPlaces").post(verifyJWT, addPlaces);
+router.route("/addPlaces").post(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "photos",
+      maxCount: 8,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  addPlaces
+);
 
 export default router;
